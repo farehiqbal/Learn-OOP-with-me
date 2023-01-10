ff9ab520d569cdbe4e3bdbef4386c77187f40c2e
Fareh Iqbal <fareh.legend@gmail.com>
2023-01-10 14:07:58 +0500

    Added Date utility

---
+++ Project/Utils/Date.java
@@ -0,0 +1,26 @@
+package Project.Utils;
+
+public class Date {
+    private int year;
+    private int month;
+    private int day;
+
+    public Date(int day, int month, int year) {
+        this.year = year;
+        this.month = month;
+        this.day = day;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public int getMonth() {
+        return month;
+    }
+
+    public int getDay() {
+        return day;
+    }
+}
+
