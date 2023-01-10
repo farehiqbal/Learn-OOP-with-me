b6697c9c4447b5d74c624216e537ff274a059999
Fareh Iqbal <fareh.legend@gmail.com>
2023-01-11 04:35:17 +0500

    Run

--- Project/Inventory/Runner.java
+++ Project/Inventory/Runner.java
@@ -1,5 +1,6 @@
 package Project.Inventory;
 
+import java.io.IOException;
 import java.util.HashMap;
 
 import Project.FileHandling.FileOperation;
@@ -7,14 +8,17 @@ import Project.Products.*;
 import Project.Utils.Date;
 
 public class Runner {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException, ClassNotFoundException {
         
-        FileOperation<HashMap<Product,Integer>> file = new FileOperation<>("Inventory");
+        FileOperation<Inventory> file = new FileOperation<>("Project\\DataBase\\Inventory");
 
         Inventory inventory = new Inventory(10);
         
         // inventory.addIntoInventory(new Apple(1.0, new Date(10, 02, 2023), 100), 50);
-        inventory.updateQuantity(new Apple("Apple"), 20);
-        inventory.printInventory();
+        // inventory.updateQuantity(new Apple("Apple"), 20);
+        // inventory.printInventory();
+
+        // file.push(inventory);
+        file.pull().printInventory();
     }
 }
