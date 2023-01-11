fa0eb5d6896b5440e9f9d861afb4061484e3f6bb
Fareh Iqbal <fareh.legend@gmail.com>
2023-01-11 19:47:18 +0500

    run run

--- Project/FileHandling/Runner.java
+++ Project/FileHandling/Runner.java
@@ -1,21 +1,37 @@
 package Project.FileHandling;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.HashMap;
 
+import Project.Store;
 import Project.Catalog.Cataloge;
 import Project.Products.*;
 
 public class Runner {
     public static void main(String[] args) throws ClassNotFoundException, IOException {
         
-        FileOperation<HashMap<Product,Boolean>> file = new FileOperation<>("Project\\DataBase\\ProductCatalog");
+        // FileOperation<HashMap<Product,Boolean>> file = new FileOperation<>("Project\\DataBase\\ProductCatalog");
 
-        file.pull().entrySet().forEach(entry -> {
-            System.out.println(entry.getKey() + " " + entry.getValue());
-        });
+        // file.pull().entrySet().forEach(entry -> {
+        //     System.out.println(entry.getKey() + " " + entry.getValue());
+        // });
         
         // HashMap<Product,Boolean> cataloge = Cataloge.readyMadeCatalog();
         // file.push(cataloge);
+
+        FileOperation<ArrayList<Store>> file  = new FileOperation<>("Project//DataBase//Store");
+
+        // Store store = file.pull();
+        // store.printStoreInventory();
+
+        // ArrayList<Store> stores = file.pullIterable();
+        
+        
+        ArrayList<Store> stores = file.pull();
+        for (Store store : stores) {
+            store.displayStoreInventory();
+            store.getLocation();
+        }
      }
 }
---
+++ Project/Panels/Runner.java
@@ -0,0 +1,10 @@
+package Project.Panels;
+
+public class Runner {
+    
+    public static void main(String[] args) {
+        
+        LoginPanel panel = new LoginPanel();
+        panel.setVisible(true);
+    }
+}
