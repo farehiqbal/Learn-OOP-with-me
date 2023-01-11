ad34876a8b59e2a58071e1aa5964e00e7e6d4707
Fareh Iqbal <fareh.legend@gmail.com>
2023-01-11 19:47:08 +0500

    Customer class updated

--- Project/Users/Customer.java
+++ Project/Users/Customer.java
@@ -10,6 +10,10 @@ public class Customer extends User implements Serializable{
 
     }
 
+    public Customer(String location){
+        this.location = location;
+    }
+
     public Customer(String cnic, String location, String pass, String confirmPass, String gender, String address, long phone) {
         this.cnic = cnic;
         this.password = pass;
@@ -18,10 +22,6 @@ public class Customer extends User implements Serializable{
         this.address = address;
         this.phone = phone;
         this.location = location;
-    }
-
-    public Customer(String confirmPass) {
-        this.confirmPass = confirmPass;
     }
 
     public String getCnic() {
