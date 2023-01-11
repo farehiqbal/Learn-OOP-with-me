c2e6bd360a68bdb5f6639b1afd1c47f5546137cd
Fareh Iqbal <fareh.legend@gmail.com>
2023-01-11 19:47:32 +0500

    This class does the magic

--- Project/FileHandling/FileOperation.java
+++ Project/FileHandling/FileOperation.java
@@ -1,11 +1,16 @@
 package Project.FileHandling;
 
 import java.io.*;
+import java.util.ArrayList;
 
-public class FileOperation<T> {
+public class FileOperation<T> implements Serializable{
 
     String path;
     File file;
+
+    FileOperation(){
+
+    }
 
     public FileOperation(String path) {
         this.path = path + ".ser";
@@ -26,5 +31,46 @@ public class FileOperation<T> {
         stream.close();
         
         return object;
+    }
+
+
+    public boolean delete(T object) throws IOException, ClassNotFoundException {
+        
+        ObjectInputStream stream = new ObjectInputStream(new FileInputStream(file));
+        @SuppressWarnings("unchecked")
+        T del = (T) stream.readObject();
+        stream.close();
+        
+        boolean flag= false;
+        ArrayList<T> list = pullIterable();
+        if(list.remove(del)) flag = true;
+        
+        for (T objects : list) {
+            push(objects);
+        }
+        return flag;
+    }
+
+    public ArrayList<T> pullIterable(){
+        
+        ArrayList<T> list = new ArrayList<>();
+
+        try {
+            ObjectInputStream ois = new ObjectInputStream(new FileInputStream(file));
+            while (true) {
+                @SuppressWarnings("unchecked")
+                T s = (T) ois.readObject();
+                list.add(s);
+            }
+        } catch (ClassNotFoundException e) {
+            // System.out.println("ClassNotFoundException");
+        } catch (EOFException e) {
+            // System.out.println("EOFException");
+        } catch (IOException e) {
+            // System.out.println("IOException");
+        }
+        return list;
+
+        
     }
 }
